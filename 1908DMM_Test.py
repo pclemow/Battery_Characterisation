cf790dc807744b519a4fc5835099f8a636c52fff
pclemow <philip.clemow@imperial.ac.uk>
2026-09-21 13:02:31 +0100

    Updated testing voltage range

--- 1908DMM_Test.py
+++ 1908DMM_Test.py
@@ -110,9 +110,17 @@ DMM1908.write("IDC2 10A\n".encode())
 
 DMM1908.write("Read?\n".encode())
 time.sleep(0.2)
+print("Received " + DMM1908.in_waiting + "characters")
 
 received_data = DMM1908.read(DMM1908.in_waiting).decode()
-print(f"Received: {received_data.strip()}")
+print(f"Received from 1: {received_data.strip()}")
+
+DMM1908.write("Read2?\n".encode())
+time.sleep(0.2)
+print("Received " + DMM1908.in_waiting + "characters")
+
+received_data = DMM1908.read(DMM1908.in_waiting).decode()
+print(f"Received from 2: {received_data.strip()}")
 
 DMM1908.close()
 print("Serial port closed.")
